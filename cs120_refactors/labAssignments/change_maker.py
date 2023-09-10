2741231a7dbd097fe635d5cf544f4eb2cc45b274
Brahm V <brahmvanh@gmail.com>
2023-09-09 20:25:48 -0400

    refactored change maker with cents

--- cs120_refactors/labAssignments/change_maker.py
+++ cs120_refactors/labAssignments/change_maker.py
@@ -7,6 +7,24 @@ cents = int(input())
 
 # Report the number of coins we will give out to make that change.
 
-change_cents = coins.number_of_coins(cents)
-print(change_cents)
+number_of_coins = coins.number_of_coins(cents)
+print(f"you will be returning {number_of_coins} coins")
+
+change_cents = 0
+
+while (change_cents + 25 < cents):
+  coins.dispense_quarter(True)
+  change_cents += 25
+
+while (change_cents + 10 < cents):
+  coins.dispense_dime(True)
+  change_cents += 10
+
+while (change_cents + 5 < cents):
+  coins.dispense_nickel(True)
+  change_cents += 5
+
+while (change_cents < cents):
+  coins.dispense_penny(True)
+  change_cents += 1
 
--- cs120_refactors/labAssignments/coins.py
+++ cs120_refactors/labAssignments/coins.py
@@ -11,44 +11,48 @@ class Coins():
      # @return the number of coins required to make change for the given number of cents.
     def number_of_coins(self, cents):
           ret = 0
-          ret += cents / 25
+          ret += cents // 25
           cents %= 25
-          ret += cents / 10
+          ret += cents // 10
           cents %= 10
-          ret += cents / 5
+          ret += cents // 5
           cents %= 5
           ret += cents 
-          return cents
+          return ret
      
-    def dispense_quarter(self, beep):
+    def dispense_quarter(self, beep: bool):
           print("/--\\")
           print("|25|")
           print("\\--/\n")
           if (beep):
-               beep()
+               self.beep()
 
-    def dispense_dime(self, beep):
+    def dispense_dime(self, beep: bool):
          print("/--\\")
          print("|10|")
          print("\\-/\n")
          if (beep):
-              beep()
+              self.beep()
     
-    def dispense_nickel(self, beep):
+    def dispense_nickel(self, beep: bool):
          print("/--\\")
          print("|5|")
          print("\\-/\n")
+         if (beep):
+              self.beep()
     
-    def dispense_penny(self, beep):
+    def dispense_penny(self, beep: bool):
          print("/-\\")
          print("|1|")
          print("\\-/\n")
+         if (beep):
+              self.beep()
 
 
-    def beep():
+    def beep(self):
           print("\007")
           try:
-               time.sleep(333)
+               time.sleep(1)
           except: 
                print("An exception occured")
                
